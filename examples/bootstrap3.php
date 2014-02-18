cb7807842eb5d27f32bae930b4236772e927ac84
Plamen Popov <tzappa@gmail.com>
2014-02-18 13:57:15 +0200

    bootstrap3 pager example

--- examples/bootstrap3.php
+++ examples/bootstrap3.php
@@ -1,35 +1,59 @@
 <?php
 
-$string = '<ul class="pagination">';
-foreach ($pages as $key => $page) {
-	// link
-	$href = $page["uri"];
+function renderPagination(array $pages)
+{
+	$string = '<ul class="pagination">';
+	foreach ($pages as $key => $page) {
+		// link
+		$href = $page["uri"];
 
-	$rel = "";
-	// label
-	if ($key === "prev") {
-		$label = "&laquo;";
-		$rel = ' rel="prev"';
-	} elseif ($key === "next") {
-		$label = "&raquo;";
-		$rel = ' rel="next"';
-	} elseif ($key === "less" or $key === "more") {
-		$label = "...";
-	} else {
-		$label = $page["page"];
-	}
+		$rel = "";
+		// label
+		if ($key === "prev") {
+			$label = "&laquo;";
+			$rel = ' rel="prev"';
+		} elseif ($key === "next") {
+			$label = "&raquo;";
+			$rel = ' rel="next"';
+		} elseif ($key === "less" or $key === "more") {
+			$label = "...";
+		} else {
+			$label = $page["page"];
+		}
 
-	// class
-	if ($page["isCurrent"]) {
-		$class = "active";
-		$string .= '<li class="active"><a'.$rel.' href="'.$href.'">'.$label. '<span class="sr-only">(current)</span></a></li>';
-	} elseif ($page["isDisabled"]) {
-		$string .= '<li class="disabled"><span>'.$label.'</span></li>';
-	} else {
-		$string .= '<li><a'.$rel.' href="'.$href.'">'.$label.'</a></li>';
+		// class
+		if ($page["isCurrent"]) {
+			$class = "active";
+			$string .= '<li class="active"><a'.$rel.' href="'.$href.'">'.$label. '<span class="sr-only">(current)</span></a></li>';
+		} elseif ($page["isDisabled"]) {
+			$string .= '<li class="disabled"><span>'.$label.'</span></li>';
+		} else {
+			$string .= '<li><a'.$rel.' href="'.$href.'">'.$label.'</a></li>';
+		}
+
 	}
+	$string .= '</ul>';
+
+	return $string;
+}
+
+function renderPager(array $pages)
+{
+	$string = '<ul class="pager">';
+	$string .= '<li rel="prev"'.($pages["prev"]["isDisabled"] ? 'class="disabled"' : '').'><a href="'.$pages["prev"]["uri"].'">Previous</a></li>';
+	$string .= '<li rel="next"'.($pages["next"]["isDisabled"] ? 'class="disabled"' : '').'><a href="'.$pages["next"]["uri"].'">Next</a></li>';
+	$string .= '</ul>';
+
+	return $string;
+}
+
+function renderAlignedPager(array $pages)
+{
+	$string = '<ul class="pager">';
+	$string .= '<li class="previous'.($pages["prev"]["isDisabled"] ? ' disabled' : '').'"><a href="'.$pages["prev"]["uri"].'">&larr; Older</a></li>';
+	$string .= '<li class="next'.($pages["next"]["isDisabled"] ? ' disabled' : '').'"><a href="'.$pages["next"]["uri"].'">Newer &rarr;</a></li>';
+	$string .= '</ul>';
+
+	return $string;
 
 }
-$string .= '</ul>';
-
-return $string;
--- examples/index.php
+++ examples/index.php
@@ -3,17 +3,27 @@
 use SugiPHP\Pagination\Pagination;
 
 include "../vendor/autoload.php";
+include "bootstrap3.php";
 
 $pagination = new Pagination();
 $pagination->setTotalItems(105)->setProximity(2)->setItemsPerPage(10); // same as setLimit()
-$pages = $pagination->toArray();
-$bs3pagination = include "bootstrap3.php";
 ?>
 <html>
 <head>
 	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet" />
+<style>
+body {
+	max-width: 580px;
+	margin: 20px auto;
+}
+</style>
 </head>
 <body>
-<?=$bs3pagination;?>
+<h3>Bootstrap3 Pagination</h3>
+<?= renderPagination($pagination->toArray()); ?>
+<h3>Bootstrap3 Pager</h3>
+<?= renderPager($pagination->toArray()); ?>
+<h3>Bootstrap3 Aligned Pager</h3>
+<?= renderAlignedPager($pagination->toArray()); ?>
 </body>
 </html>
